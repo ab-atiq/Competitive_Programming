a8aabd143c84e36da4a495e5d8a28a821972a05f
Abul Bashar Atiq <91819259+ab-atiq@users.noreply.github.com>
2022-08-26 10:43:43 +0600

    Create 71A.c

---
+++ Codeforces/71A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int tc;
+    scanf("%d", &tc);
+
+    for (int i = 0; i < tc; i++)
+    {
+        char str[101];
+        scanf("%s", str);
+        int len = strlen(str);
+        if (len <= 10)
+        {
+            printf("%s\n", str);
+        }
+        else
+        {
+            printf("%c%d%c\n", str[0], len - 2, str[len-1]);
+        }
+    }
+
+    return 0;
+}
