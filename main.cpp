215136bae6298c7a597f5c980eed2f9dcaeec158
atiqur rahman <abulbasharatiqur@gmail.com>
2024-09-12 07:36:37 +0600

    exe file delete

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 // --------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
 int main()
 {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
     int tc;
     cin >> tc;
-    for (int i = 1; i <= tc; i++)
+    for (int t = 1; t <= tc; t++)
     {
-        }
+        int n;
+        cin >> n;
+        cout << n << endl;
+    }
     return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -5,16 +5,17 @@ using namespace std;
 // Driver code
 int main()
 {
-    bool onOroff = true;
+    bool onOff = true;
     float a = 23.333;
     char s[] = "GeeksForGeeksdfssafsdfgsdfgsdfgsdfgdsfgsdf", s1[] = {"aaa"};
     // strcat(s, "a");
     cout << s << endl;
     cout << s1;
-    string str="abul bashar";
+    string str = "abul bashar";
     // str.swap()
-    cout<<str.max_size()<<endl;
-    int x,y,z;
-    x=y=z=4;
+    cout << str.max_size() << endl;
+    int x, y, z;
+    x = y = z = 4;
+
     return 0;
 }
